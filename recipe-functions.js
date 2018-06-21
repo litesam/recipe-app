2a32e25ab939b2b766abd085cd255b5cf9f1461e
litesam <lightsam126@gmail.com>
2018-06-21 14:45:08 +0530

    Fixed: Small note to show is items are available.

--- recipe-functions.js
+++ recipe-functions.js
@@ -18,19 +18,19 @@ const saveRecipes = (recipes) => {
 }
 
 // Needs to be updated: Shows how many content we have in the given individual recipe!
-const showParagraph = (indItems, recipes) => {
+const showParagraph = (indItems, recipe) => {
     
-    // // const recipes = getSavedRecipes()
-    // const el = indItems.filter((element) => {
-    //     return element.checkItem
-    // })
-    // console.log(recipes.requiredItems)
-    // console.log(el)
-    // // if (el.length === recipes.requiredItems.length) {
-    // //     return 'You have all the items!'
-    // // } else if (el.length < recipes.requiredItems.length) {
-    // //     return 'You have some of the items!'
-    // // } else {
-    // //     return 'You have none of the items!'
-    // // }
+    
+    const el = indItems.filter((element) => {
+        return element.checkItem
+    })
+    console.log(recipe.requiredItems)
+    console.log(el)
+    if (el.length === recipe.requiredItems.length) {
+        return 'You have all the items!'
+    } else if (el.length < recipe.requiredItems.length) {
+        return 'You have some of the items!'
+    } else {
+        return 'You have none of the items!'
+    }
 }
